72705131fe2fc835e583330032f0601383d8d636
nishideokar.sits.it@gmail.com <nishideokar.sits.it@gmail.com>
2024-08-01 11:13:13 +0530

    updated

--- src/pages/Allstores.jsx
+++ src/pages/Allstores.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StoreCard from "../components/storecard/StoreCard";
+import allstore from '../data/Allstores.json'
 
 const Allstores = () => {
   return (
@@ -7,13 +8,23 @@ const Allstores = () => {
       <div className="container">
         <h1>All stores in pune</h1>
       </div>
-      <div className="store-card-container">
-        <StoreCard />
-        <StoreCard />
-        <StoreCard />
-      </div>
+
+      {
+  allstore.map((val, index) => {
+    return (
+      <>
+        <div className="store-card-container" key={index}>
+          <StoreCard img={val.img} name={val.storename} area={val.address} dist={val.distance} />
+        </div>
+      </>
+    );
+  })
+}
     </>
   );
 };
 
 export default Allstores;
+
+
+  
