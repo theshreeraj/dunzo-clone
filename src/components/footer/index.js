c519f4835c6c943fe57b07df5e13f956945ae082
Shreeraj Mane <shreerajmane007@gmail.com>
2024-12-20 11:26:37 +0530

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,6 +4,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="footer flex absolute-center">
+    <p> We are aiming to make this project as a complete fullstack project including all the features, mainly dashboard & payment gateway stay tuned.</p>
       Made with 🔥 by Decoders Club - theshreeraj
     </div>
   );
