b39aff8f3b332312648cccd974f798de51b17d31
theshreeraj <shreerajmane007@gmail.com>
2024-12-20 11:39:44 +0530

    [update]:footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,10 +3,14 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer flex absolute-center">
-    <p> We are aiming to make this project as a complete fullstack project including all the features, mainly dashboard & payment gateway stay tuned.</p>
-      Made with 🔥 by Decoders Club - theshreeraj
-    </div>
+    <>
+      <p className="center-text border-black">
+        We are aiming to make this project as a complete fullstack project including all the features, mainly dashboard & payment gateway stay tuned.
+      </p>
+      <div className="footer flex absolute-center">
+        Made with 🔥 by Decoders Club - theshreeraj
+      </div>
+    </>
   );
 };
 
